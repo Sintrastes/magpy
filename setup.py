875e321a5affe7754a17775403007632b77b57d6
Nathan BeDell <nate@Nate-Aspire-one.(none)>
2014-06-08 16:00:31 -0400

    added setup file

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+from distutils.core import setup
+
+setup(
+    description = 'A python library for dealing with magmas.',
+    author = 'Nathan BeDell',
+    url = 'https://github.com/Sintrastes/magpy',
+    download_url = 'https://github.com/Sintrastes/magpy.git',
+    author_email = 'nabedell@liberty.edu',
+    version = '0.1',
+    install_requires = ['abc'],
+    packages = ['magpy'],
+    scripts = [],
+    name = 'magpy',
+    classifiers = ["Programming Language :: Python :: 3"])
+
+
+
